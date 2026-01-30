a42dfbe0e409a65b9d1bb417851047eaffe4ee01
Abir Hasan <abirhasanrawvy@gmail.com>
2026-01-30 23:30:01 +0600

    DOM.
    Here is discuss about the DOM manupulation basic.

--- DOM.js
+++ DOM.js
@@ -1,1 +1,13 @@
 //DOM Manupulation: Document Object Model;
+console.log("====================================");
+
+let titleElement = document.getElementById("title");
+
+console.log(titleElement);
+
+//another one;
+let titleElement1 = document.getElementById("title1");
+
+titleElement1.textContent = "New JavaScript";
+
+console.log("====================================");
