c635ca76f0710ba6a1833c1d9b9c0938cc213a66
Abir Hasan <abirhasanrawvy@gmail.com>
2026-01-31 23:27:03 +0600

    GetAttribute & SetAttribute.
    Here, discuss about the get and set attribute and advance practice.

--- getSetAttribute.js
+++ getSetAttribute.js
@@ -1,1 +1,14 @@
-//getAttribute;
+//getAttribute: knowing for attribute value;
+
+let link = document.getElementById("myLink");
+let hrefValue = link.getAttribute("href");
+console.log(hrefValue);
+
+//setAttribute:;
+const image = document.getElementById("mainImage");
+
+const oldSRC = image.getAttribute("src");
+
+console.log(oldSRC);
+
+image.setAttribute("src","https://www.elabedu.eu/wp-content/webp-express/webp-images/uploads/2025/01/view-of-rosenlaui-with-male-tourist-walking-along-2024-04-01-22-33-37-utc-1-1024x683.jpg.webp");
