252c2643d65b5b851514f4ecab710fb40632c7fa
Abir Hasan <abirhasanrawvy@gmail.com>
2026-01-31 14:33:21 +0600

    Modify Element.
    Here, innerText and textContent practiced.

--- modifyElement.js
+++ modifyElement.js
@@ -1,1 +1,11 @@
 //Modify Element;
+
+//textContent: Change the whole text;
+let demo = document.getElementById("demo");
+console.log(demo.textContent);
+
+demo.textContent = "Hello!";
+
+//innerText:Same but ;
+let demo1 = document.getElementById("info");
+console.log(demo1.innerText);
