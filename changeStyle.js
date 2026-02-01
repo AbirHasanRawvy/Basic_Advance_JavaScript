06c26d3349147ee7e0c3f1aeaf52dc8dc55deae8
Abir Hasan <abirhasanrawvy@gmail.com>
2026-02-01 22:17:14 +0600

    Change Style.
    Here, practice the change style of a paragraph with basic rules and with advance javaScript.

---
+++ changeStyle.js
@@ -0,0 +1,10 @@
+//Change Style.
+function changeStyle(){
+    let para = document.getElementById("myPara");
+
+    para.style.color = "red";
+    para.style.fontSize = "60px";
+    para.style.backgroundColor = "green";
+    para.style.padding = "10px";
+    para.style.border = "4px solid red";
+}
