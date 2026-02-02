b64bc4937c095c712875fd54888b2e8a5574060c
Abir Hasan <abirhasanrawvy@gmail.com>
2026-02-02 12:11:27 +0600

    Add Remove Class.
    here, practice on add remove item with 2 types methods.

--- addRemoveClass.js
+++ addRemoveClass.js
@@ -1,1 +1,15 @@
 //Adding and Removing class;
+
+// //for add;
+// element.classList.add("class-name");
+
+// //for remove;
+// element.classList.remove("class-name");
+
+// //toogle;
+// element.classList.toggle("class-name");
+
+// //contains;
+// element.classList.contains("class-name");
+
+//
