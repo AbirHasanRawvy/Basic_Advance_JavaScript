9ea3c115b8e0a49f2b91cdee357585ded50f2f58
Abir Hasan <abirhasanrawvy@gmail.com>
2026-02-06 22:45:56 +0600

    Class1.
    Here is practice on "Object" and one is dot notation another is bracket notation.

--- class1.js
+++ class1.js
@@ -1,3 +1,31 @@
 // JavaScript Object & JSON | Arrays and Array Methods | 
 // Strings and String Operations | JavaScript Date | 
 // Math | Number | Window object | Navigator object
+
+//Object;
+const myName = "Abir Hasan";
+
+let student = {
+    name: "Abir",
+    age: 20,
+    subjects: ["TOC","WebTec","AddWeb"],
+    "full name" : "Abir Hasan",
+};//2 way of object asscess: 1. dot donation, 2. bracket notation;
+//dot notation;
+console.log(student.name);
+console.log(student.age);
+//Bracket notation;
+console.log(student["full name"]);
+
+let calculator = {
+    x: 10,
+    y: 20,
+    add: function () {
+        return this.x + this.y; 
+    },
+    mul: function () {
+        return this.x * this.y;
+    },
+};
+console.log(calculator.add());
+console.log(calculator.mul());
