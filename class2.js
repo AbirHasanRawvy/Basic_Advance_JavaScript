7c8ed54035deb60f864411aed6a29fc64d31fc3a
Abir Hasan <abirhasanrawvy@gmail.com>
2026-02-15 22:49:04 +0600

    Class2.
    Here, clear the concept of getElementById and getElementByName further more about practice on the querySelector.

--- class2.js
+++ class2.js
@@ -2,3 +2,22 @@
 // DOM manipulation | Form handling | DOM element creation | 
 // Appending elements | Query selector
 console.log("Hello World!");
+
+//When we take something from html file, if select id then use getElementById
+
+const title = document.getElementById("main-title");
+console.log(title);
+
+//On the other hand, if we select the class then use getElemenByName;
+const paragraph = document.getElementsByClassName("intro");
+console.log(paragraph);
+
+//Query Selector: Query selector only select the first intro value further 
+//the . use for class and # use for id;
+//for class;
+const paragraph1 = document.querySelector(".intro");
+console.log(paragraph1);
+
+//for id;
+const paragraph2 = document.querySelector("#main-title");
+console.log(paragraph2);
