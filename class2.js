b7c28f65dd39f53ca76dfe67fc0e63fabadf6df5
Abir Hasan <abirhasanrawvy@gmail.com>
2026-02-16 22:44:22 +0600

    Class2.
    Here, practice on querySelectorAll, innerText, innerHTML, change the title colour with JS and  event listen with click.

--- class2.js
+++ class2.js
@@ -21,3 +21,29 @@ console.log(paragraph1);
 //for id;
 const paragraph2 = document.querySelector("#main-title");
 console.log(paragraph2);
+
+//change the inner text value;
+paragraph[0].innerText = "Welcome to DOM manupulation!";
+console.log(paragraph);
+
+paragraph1.innerText = "We are continue DOM manupulation!";
+console.log(paragraph1);
+
+//innerHTML;
+paragraph2.innerHTML = "<strong>We are continue DOM manupulation!</strong>";
+console.log(paragraph2);
+
+//change the title colour;
+title.style.color = "blue";
+title.style.backgroundColor = "yellow";
+
+//querySelectorAll: same as get elementByName;
+
+//
+const myButton = document.getElementById("myButton");
+myButton.addEventListener("click", function () {
+    title.textContent = "Hi from JS!";
+    title.style.color = "red";
+    title.style.backgroundColor = "yellow";
+});//all the event listen;
+console.log(myButton);
